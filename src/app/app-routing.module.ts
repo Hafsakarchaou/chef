fc9580418e31ca2325141728d52dbca2a553b418
HafsaKarchaou <hafsadiy@gmail.com>
2022-04-30 12:06:02 +0000

    m1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemandesComponent } from './demandes/demandes.component';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:ProfilComponent},
+  {path:"demandes", component:DemandesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebrComponent } from './sidebr/sidebr.component';
+import { ProfilComponent } from './profil/profil.component';
+import { DemandesComponent } from './demandes/demandes.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebrComponent,
+    ProfilComponent,
+    DemandesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
